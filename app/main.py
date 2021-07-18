ce666f6cd630bc99034ceb4d80a5eb61d3e196d2
Ricardo Miranda <ricardoMiranda.partner@auchan.pt>
2021-07-18 21:07:11 +0100

    fucntion that computes floor of division of squares

---
+++ app/main.py
@@ -0,0 +1,2 @@
+def floor_division_squares(x: int, y: int) -> int:
+    return (y*y) // (x*x)
--- tests/test_main.py
+++ tests/test_main.py
@@ -4,4 +4,30 @@ from app import main
 
 
 class TestMain(unittest.TestCase):
-    pass
+    def test_floor_division_squares_00(self) -> None:
+        actual: int = main.floor_division_squares(x=1, y=1)
+        expected: int = 1
+
+        self.assertEquals(first=expected, second=actual)
+
+    def test_floor_division_squares_01(self) -> None:
+        self.assertRaises(ZeroDivisionError,
+                          main.floor_division_squares, x=0, y=0)
+
+    def test_floor_division_squares_02(self) -> None:
+        actual: int = main.floor_division_squares(x=2, y=2)
+        expected: int = 1
+
+        self.assertEquals(first=expected, second=actual)
+
+    def test_floor_division_squares_03(self) -> None:
+        actual: int = main.floor_division_squares(x=1, y=2)
+        expected: int = 4
+
+        self.assertEquals(first=expected, second=actual)
+
+    def test_floor_division_squares_04(self) -> None:
+        actual: int = main.floor_division_squares(x=2, y=3)
+        expected: int = 2
+
+        self.assertEquals(first=expected, second=actual)
